84c0a9bbe1921cc520f82e627c7c9eecceebea1d
zongjuZhao <819031062@qq.com>
2021-03-19 20:24:16 +0800

    add check cookies

--- src/main/java/life/nefu/community/controller/AuthorizeController.java
+++ src/main/java/life/nefu/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -41,7 +43,8 @@ public class AuthorizeController {
 //  1.得到/callback得到code
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
 //   2.accessToek携带code
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -56,14 +59,18 @@ public class AuthorizeController {
         System.out.println(githubUser.getName() );
         if(githubUser!=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+//          不用JSESSIONID，自己生成token
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             //5.登陆成功，写cookie和session
-            request.getSession().setAttribute("user",user);
+//            request.getSession().setAttribute("user",user);
+//            优化；token写入cookie
+            response.addCookie(new Cookie("token", token));
             //5.重定向主页面：注意redirect返回根目录/，不是index
             return "redirect:/";
         }else{
